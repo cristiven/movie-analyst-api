6eb7f93f7ffa2bafeff1ace652204ec36273a309
Cristian Vargas Rojo <cristian.vargasrojo@dfinsolutions.com>
2022-02-24 09:23:57 -0500

    change host to connection DB

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 //------------
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
+  host     : process.env.DB_HOST || '192.168.1.237',
   user     : process.env.DB_USER || 'root',
   password : process.env.DB_PASS || 'root',
   database : process.env.DB_NAME || 'movie_db'
